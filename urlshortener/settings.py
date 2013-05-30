9333002f31d10ed57769be391565718478368244
Cassio Cabral <cassioscabral@gmail.com>
2013-05-30 17:23:12 -0300

    retirado o ajax que estava com problemas

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -19,14 +19,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test_shortener_sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.dummy', 
+                   
     }
-
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
