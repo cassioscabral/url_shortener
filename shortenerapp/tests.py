54e17ae464b293abe426c3056fbf57a4b69420e0
Cassio Cabral <cassioscabral@gmail.com>
2013-05-30 11:38:33 -0300

    testes sao executados mas procura por um banco relacional ao inves de usar o mongodb

--- shortenerapp/tests.py
+++ shortenerapp/tests.py
@@ -1,16 +1,48 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+#coding: utf-8
+from nose.plugins.skip import SkipTest
+ 
+from mongoengine.python_support import PY3
+from mongoengine import connect
+ 
+try:
+    from django.test import TestCase
+    from django.conf import settings
+except Exception as err:
+    if PY3:
+        from unittest import TestCase
+        # Dummy value so no error
+        class settings:
+            DBNAME = 'dummy'
+    else:
+        raise err
+ 
+ 
+class MongoTestCase(TestCase):
+    """
+        TestCase class that clear the collection between the tests
+    """
+    mongodb_name = 'test_%s' % settings.DBNAME
+    
+    def _pre_setup(self):
+        if PY3:
+            raise SkipTest('django does not have Python 3 support')
+ 
+        from mongoengine.connection import connect, disconnect
+        disconnect()
+        connect(self.mongodb_name, port=settings.MONGO_PORT)
+        super(MongoTestCase, self)._pre_setup()
 
-Replace this with more appropriate tests for your application.
-"""
+    def setUp(self):
+        Url.objects.create(url_original="https://www.google.com.br/")
+        num_caracteres_link = "soda/".count #5 caracteres, padrao da url reduzida sao 6 caracteres
 
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_numero_de_letras(self):
+        google = Url.objects.get(name="https://www.google.com.br/")
+        self.assertEqual(google.url_modificada.count, num_caracteres_link+6)
+ 
+    def _post_teardown(self):
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database(self.mongodb_name)
+        disconnect()
+        super(MongoTestCase, self)._post_teardown()
